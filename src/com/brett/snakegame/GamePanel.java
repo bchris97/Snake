f2bb11ca4d5ef86f2a9549820b94793f7047aaa2
Brett Stalcup <brettstalcup@gmail.com>
2024-01-31 20:30:11 -0600

    grid

--- src/com/brett/snakegame/GamePanel.java
+++ src/com/brett/snakegame/GamePanel.java
@@ -13,22 +13,55 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT/UNIT_SIZE);
 	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten = 0;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
 	
 	
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	
 	public void startGame() {
-		
+		newApple();
+		running = true;
+		timer = new Timer(DELAY,this);
+		timer.start();
 	}
 	
 	public void paintComponent(Graphics g) {
-		
+		super.paintComponent(g);
+	    System.out.println("Painting"); // Add this line
+
+		draw(g);
 	}
 	
 	public void draw(Graphics g) {
 		
+		g.setColor(Color.WHITE);
+		
+		for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+
+		}
+	}
+	
+	public void newApple() {
+		appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+	    appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 	}
 	
 	public void move() {
